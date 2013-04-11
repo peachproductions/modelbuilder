d3fbc5ee791b6e6e64dde93de07ddd07b485bb21
Chris Matheson <chris@matheson.it>
2013-04-11 14:03:56 -0400

    reworked for express server

---
+++ app.js
@@ -0,0 +1,36 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.use(express.logger());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'peachproductions' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+app.get('/', function (req, res) {
+    res.redirect('/index.html');
+});
+
+app.listen(process.env.PORT);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
